44c755eac1db32cc0251260faf15c7e40a46a392
Sara Mohamed <smohamedamer4@gmail.com>
2022-02-24 09:09:35 +0200

    updates in controllers for lang

--- app/Http/Requests/Api/Language/LanguageApiRequest.php
+++ app/Http/Requests/Api/Language/LanguageApiRequest.php
@@ -4,13 +4,25 @@ namespace App\Http\Requests\Api\Language;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class LanguageApiRequest  extends FormRequest {
+class LanguageApiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
     public function authorize()
-       {
-           return true;
-       }
-       
-    public function rules( ) {
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
         return [
             'name'       =>  [ 'required' , 'max:2'     , 'unique:languages,name'      ] ,
             'full_name'  =>  [ 'required' , 'max:20'    , 'unique:languages,full_name' ] ,
