96c8a63254dedac1f011a0b71525b4f79cc4a414
Sara Mohamed <smohamedamer4@gmail.com>
2022-08-24 11:53:51 +0200

    add dashboard apis

---
+++ app/Http/Resources/Dashboard/Collections/ControllerResources/SubUserCollection/SubUserCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Dashboard\Collections\ControllerResources\SubUserCollection;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+// use App\Http\Resources\Dashboard\SubUserResource as ModelResource;
+use App\Http\Resources\Dashboard\ControllerResources\SubUserController\SubUserResource as ModelResource;
+
+
+class SubUserCollection extends ResourceCollection{
+
+    public function toArray( $request ) {
+        return $this -> collection -> map( fn( $model ) => new ModelResource ( $model ) );
+    }
+
+    public function with( $request ) {
+        return [
+            'message' => 'Successful.' ,
+            'status'   => true          ,
+            'code'   => 200          ,
+        ];
+    }
+}
