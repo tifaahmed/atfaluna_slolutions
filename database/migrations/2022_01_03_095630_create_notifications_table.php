2f3d1f9ea6c3c0b30a10d85a525e25d73eac8c13
tifaahmed <35181458+tifaahmed@users.noreply.github.com>
2022-10-03 14:46:19 +0200

    fix

--- database/migrations/2022_01_03_095630_create_notifications_table.php
+++ database/migrations/2022_01_03_095630_create_notifications_table.php
@@ -15,7 +15,6 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->increments('id');//[pk]
-            $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
             $table->text('data');
